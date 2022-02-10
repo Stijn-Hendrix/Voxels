983889ec142cf91f2e2c104e5905de1f87e1e1d3
Stijn <48821681+Stijn-Hendrix@users.noreply.github.com>
2022-02-10 12:06:35 +0100

    edit single chunk

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -9,14 +9,54 @@ public class Chunk : MonoBehaviour
 
 	public MeshFilter MeshFilterDisplay;
 
+	public MeshCollider MeshCollider;
+
 	private void Start() {
 		heights = NoiseGenerator.instance.Run(transform.position);
 
-		UpdateMesh();
 	}
 
-	public void UpdateMesh() {
+	private void Update() {
+		InstantUpdateMesh();
+
+		this.enabled = false;
+	}
+
+	public void EditHeights(Vector3 globalOrigin) {
+		Vector3Int localOrigin = ToLocalOrigin(globalOrigin);
+
+		for (int x = localOrigin.x - 2; x < localOrigin.x + 2; x++) {
+
+			for (int y = localOrigin.y - 2; y < localOrigin.y + 2; y++) {
+
+				for (int z = localOrigin.z - 2; z < localOrigin.z + 2; z++) {
+					heights[indexFromCoord(x, y, z)] += 1;
+				}
+			}
+		}
+
+		RequestUpdate();
+	}
+
+	int indexFromCoord(int x, int y, int z) {
+		return x + MeshGenerator.ChunkSize * (y + MeshGenerator.ChunkSize * z);
+	}
+
+	Vector3Int ToLocalOrigin(Vector3 globalOrigin) {
+		return new Vector3Int(
+					Mathf.FloorToInt(globalOrigin.x - (transform.position.x * MeshGenerator.ChunkSize - 1)),
+					Mathf.FloorToInt(globalOrigin.y),
+					Mathf.FloorToInt(globalOrigin.z - (transform.position.z * MeshGenerator.ChunkSize - 1))
+				);
+	}
+
+	public void RequestUpdate() {
+		this.enabled = true;
+	}
+
+	void InstantUpdateMesh() {
 		Mesh mesh = MeshGenerator.instance.Run(heights);
+		MeshCollider.sharedMesh = mesh;
 		Display(mesh);
 	}
 
---
+++ Assets/Scripts/TerrainEdit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainEdit : MonoBehaviour
+{
+	Vector3 hitPoint;
+
+	private void LateUpdate() {
+
+		if (Input.GetMouseButton(0)) {
+			RaycastHit hit;
+
+
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000)) {
+				hitPoint = hit.point;
+
+				var chunk = hit.collider.GetComponentInParent<Chunk>();
+				chunk.EditHeights(hitPoint);
+			}
+		}
+	}
+
+	private void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(hitPoint, 4);
+	}
+}
